42d25d48b18d7a6a24ff2fa322ece22cda9c376f
Oussama Benhamed <wisleur@gmail.com>
2024-05-03 19:32:33 +0200

    feat(trail): try to find route name if the latter is undefined

--- src/breadcrumbs_trail.ts
+++ src/breadcrumbs_trail.ts
@@ -34,7 +34,13 @@ export class BreadcrumbsTrail {
    * Add a parent item to the trail
    */
   parent(routeName: string) {
-    this.#registry.getNamedRouteCallback(routeName)(this.#ctx, this, ...this.#resources)
+    const callback = this.#registry.getNamedRouteCallback(routeName)
+
+    if (!callback) {
+      throw new Error(`Cannot find a callback for route ${routeName}`)
+    }
+
+    callback(this.#ctx, this, ...this.#resources)
 
     return this
   }
@@ -43,6 +49,18 @@ export class BreadcrumbsTrail {
    * Add a breadcrumb item to the trail
    */
   push(title: string, url: string, name?: string) {
+    /**
+     * If name is not defined, then try to find the route name.
+     * This is helpful for BreadcrumbsRegistry.for method
+     */
+    if (!name) {
+      const route = this.#router.match(url, 'GET')
+
+      if (route) {
+        name = route.route.name
+      }
+    }
+
     this.#breadcrumbs.push({ title, url, name })
 
     return this
